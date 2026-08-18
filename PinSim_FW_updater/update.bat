00fd0ee6fbea6291f26e405d83765cf154c41153
Ben Levitt <benjie@gmail.com>
2026-08-18 09:04:17 -0700

    Add Windows support to the firmware packages.

---
+++ PinSim_FW_updater/update.bat
@@ -0,0 +1,4 @@
+@echo off
+set "TOOL_DIR=%~dp0data"
+
+"%TOOL_DIR%\esptool.exe" --chip esp32s3 --baud 921600 --before default_reset --after hard_reset write_flash -e -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x0000 "%TOOL_DIR%\bootloader.bin" 0x8000 "%TOOL_DIR%\partitions.bin" 0xe000 "%TOOL_DIR%\boot_app0.bin" 0x10000 "%TOOL_DIR%\firmware.bin"
